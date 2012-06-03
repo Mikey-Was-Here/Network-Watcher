ce045608ed7ee1e6578b7b42418f50181ae9bc70
MIchael R. Schmidt <mikey.was.here@hotmail.com>
2012-06-03 14:51:10 -0700

    Added functionallity, need to dsplay IPs next

---
+++ NetworkWatcher/Api.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NetworkWatcher
+{
+    class Api
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MIB_TCPROW_OWNER_PID
+        {
+            public uint state;
+            public uint localAddr;
+            public byte localPort1;
+            public byte localPort2;
+            public byte localPort3;
+            public byte localPort4;
+            public uint remoteAddr;
+            public byte remotePort1;
+            public byte remotePort2;
+            public byte remotePort3;
+            public byte remotePort4;
+            public int owningPid;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MIB_TCPTABLE_OWNER_PID
+        {
+            public uint dwNumEntries;
+            MIB_TCPROW_OWNER_PID table;
+        }
+
+        enum TCP_TABLE_CLASS
+        {
+            TCP_TABLE_BASIC_LISTENER,
+            TCP_TABLE_BASIC_CONNECTIONS,
+            TCP_TABLE_BASIC_ALL,
+            TCP_TABLE_OWNER_PID_LISTENER,
+            TCP_TABLE_OWNER_PID_CONNECTIONS,
+            TCP_TABLE_OWNER_PID_ALL,
+            TCP_TABLE_OWNER_MODULE_LISTENER,
+            TCP_TABLE_OWNER_MODULE_CONNECTIONS,
+            TCP_TABLE_OWNER_MODULE_ALL
+        }
+
+        [DllImport("iphlpapi.dll", SetLastError = true)]
+        static extern uint GetExtendedTcpTable(IntPtr pTcpTable, ref int dwOutBufLen, bool sort, int ipVersion, TCP_TABLE_CLASS tblClass, int reserved);
+
+        //public TcpRow[] GetAllTcpConnections()
+        public static MIB_TCPROW_OWNER_PID[] GetAllTcpConnections()
+        {
+            //  TcpRow is my own class to display returned rows in a nice manner.
+            //    TcpRow[] tTable;
+            MIB_TCPROW_OWNER_PID[] tTable;
+            int AF_INET = 2;    // IP_v4
+            int buffSize = 0;
+
+            // how much memory do we need?
+            uint ret = GetExtendedTcpTable(IntPtr.Zero, ref buffSize, true, AF_INET, TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL, 0);
+            IntPtr buffTable = Marshal.AllocHGlobal(buffSize);
+
+            try
+            {
+                ret = GetExtendedTcpTable(buffTable, ref buffSize, true, AF_INET, TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL, 0);
+                if (ret != 0)
+                {
+                    return null;
+                }
+
+                // get the number of entries in the table
+                //MibTcpTable tab = (MibTcpTable)Marshal.PtrToStructure(buffTable, typeof(MibTcpTable));
+                MIB_TCPTABLE_OWNER_PID tab = (MIB_TCPTABLE_OWNER_PID)Marshal.PtrToStructure(buffTable, typeof(MIB_TCPTABLE_OWNER_PID));
+                //IntPtr rowPtr = (IntPtr)((long)buffTable + Marshal.SizeOf(tab.numberOfEntries) );
+                IntPtr rowPtr = (IntPtr)((long)buffTable + Marshal.SizeOf(tab.dwNumEntries));
+                // buffer we will be returning
+                //tTable = new TcpRow[tab.numberOfEntries];
+                tTable = new MIB_TCPROW_OWNER_PID[tab.dwNumEntries];
+
+                //for (int i = 0; i < tab.numberOfEntries; i++)        
+                for (int i = 0; i < tab.dwNumEntries; i++)
+                {
+                    //MibTcpRow_Owner_Pid tcpRow = (MibTcpRow_Owner_Pid)Marshal.PtrToStructure(rowPtr, typeof(MibTcpRow_Owner_Pid));
+                    MIB_TCPROW_OWNER_PID tcpRow = (MIB_TCPROW_OWNER_PID)Marshal.PtrToStructure(rowPtr, typeof(MIB_TCPROW_OWNER_PID));
+                    //tTable[i] = new TcpRow(tcpRow);
+                    tTable[i] = tcpRow;
+                    rowPtr = (IntPtr)((long)rowPtr + Marshal.SizeOf(tcpRow));   // next entry
+                }
+
+            }
+            finally
+            {
+                // Free the Memory
+                Marshal.FreeHGlobal(buffTable);
+            }
+
+
+
+            return tTable;
+        }
+    }
+}
---
+++ NetworkWatcher/frmMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetworkWatcher
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+             Api.MIB_TCPROW_OWNER_PID[] cn = Api.GetAllTcpConnections();
+             foreach (Api.MIB_TCPROW_OWNER_PID item in cn)
+             {
+                 ListViewItem lvi = new ListViewItem(item.remoteAddr.ToString());
+                 listView1.Items.Add(lvi);
+             }
+        }
+    }
+}
