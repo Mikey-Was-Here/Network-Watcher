bf265dd4fe304722ce1c48389b54bad2b1b3a1ee
MIchael R. Schmidt <mikey.was.here@hotmail.com>
2012-06-03 22:48:15 -0700

    get ip addresses

--- NetworkWatcher/frmMain.Designer.cs
+++ NetworkWatcher/frmMain.Designer.cs
@@ -41,7 +41,7 @@
             this.listView1.Size = new System.Drawing.Size(401, 336);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // frmMain
             // 
--- NetworkWatcher/frmMain.cs
+++ NetworkWatcher/frmMain.cs
@@ -6,9 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace NetworkWatcher
 {
+    [StructLayout(LayoutKind.Explicit)]
+    struct ip4
+    {
+        [FieldOffset(0)]
+        public byte b1;
+        [FieldOffset(1)]
+        public byte b2;
+        [FieldOffset(2)]
+        public byte b3;
+        [FieldOffset(3)]
+        public byte b4;
+        [FieldOffset(0)]
+        public uint u;
+    }
+
     public partial class frmMain : Form
     {
         public frmMain()
@@ -18,12 +34,14 @@ namespace NetworkWatcher
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-             Api.MIB_TCPROW_OWNER_PID[] cn = Api.GetAllTcpConnections();
-             foreach (Api.MIB_TCPROW_OWNER_PID item in cn)
-             {
-                 ListViewItem lvi = new ListViewItem(item.remoteAddr.ToString());
-                 listView1.Items.Add(lvi);
-             }
+            Api.MIB_TCPROW_OWNER_PID[] cn = Api.GetAllTcpConnections();
+            foreach (Api.MIB_TCPROW_OWNER_PID item in cn)
+            {
+                ip4 i = new ip4 { u = item.remoteAddr };
+                string msg = string.Format("{0}.{1}.{2}.{3}:{4}", i.b1, i.b2, i.b3, i.b4, item.remotePort1);
+                ListViewItem lvi = new ListViewItem(msg);
+                listView1.Items.Add(msg);
+            }
         }
     }
 }
