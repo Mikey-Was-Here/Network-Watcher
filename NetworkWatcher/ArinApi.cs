30e7984adef953f11a114ee75347445a61ca6343
Michael R. Schmidt <mikey.was.here@hotmail.com>
2013-09-21 19:59:33 -0700

    added try/catch in get org name code for Arin Info

--- NetworkWatcher/ArinApi.cs
+++ NetworkWatcher/ArinApi.cs
@@ -9,18 +9,25 @@ namespace NetworkWatcher
     {
         public static string GetOrginization(IPAddress ipa)
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Clear();
-            //client.DefaultRequestHeaders.Add("Accept", "application/json");
-            client.DefaultRequestHeaders.Add("Accept", "application/xml");
-            byte[] ip = ipa.GetAddressBytes();
-            string uri = string.Format("http://whois.arin.net/rest/ip/{0}.{1}.{2}.{3}", ip[0], ip[1], ip[2], ip[3]);
-            string data = client.GetStringAsync(uri).Result;
-            data = data.Replace("<?xml-stylesheet type='text/xsl' href='http://whois.arin.net/xsl/website.xsl' ?>", string.Empty);
-            data = data.Replace("<?xml version='1.0'?>", string.Empty);
-            XDocument xdoc = XDocument.Parse(data);
-            string OrgName = (from el in xdoc.Descendants() where el.Name.LocalName == "orgRef" select el.Attribute("name").Value).FirstOrDefault();
-            return OrgName;
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Clear();
+                //client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("Accept", "application/xml");
+                byte[] ip = ipa.GetAddressBytes();
+                string uri = string.Format("http://whois.arin.net/rest/ip/{0}.{1}.{2}.{3}", ip[0], ip[1], ip[2], ip[3]);
+                string data = client.GetStringAsync(uri).Result;
+                data = data.Replace("<?xml-stylesheet type='text/xsl' href='http://whois.arin.net/xsl/website.xsl' ?>", string.Empty);
+                data = data.Replace("<?xml version='1.0'?>", string.Empty);
+                XDocument xdoc = XDocument.Parse(data);
+                string OrgName = (from el in xdoc.Descendants() where el.Name.LocalName == "orgRef" select el.Attribute("name").Value).FirstOrDefault();
+                return OrgName;
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
     }
 }
